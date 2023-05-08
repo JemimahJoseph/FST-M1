3bac3c8df24f9843dc3315d2e0066450c0f45498
JemimahJoseph <132872717+JemimahJoseph@users.noreply.github.com>
2023-05-08 11:11:35 +0530

    Add files via upload

---
+++ SQL/Activities/sqlscript (1).sql
@@ -0,0 +1,62 @@
+REM   Script: Session 01
+REM   SQL-session-1-basic commands
+
+create table customers(customerid int primary key, customername varchar(20),desgination varchar(80), city varchar(50),postalcode varchar(20),country varchar(20));
+
+create table customers(customerid int primary key, customername varchar(20),desgination varchar(80), city varchar(50),postalcode varchar(20),country varchar(20));
+
+DESCRIBE customers
+
+
+insert into customers(customerid, customername,designation)values(01,'Nitya','Tech Lead');
+
+create table customers(customerid int primary key, customername varchar(20),desgination varchar(80), city varchar(50),postalcode varchar(20),country varchar(20));
+
+DESCRIBE customers
+
+
+insert into customers(customerid, customername, designation)values(01,'Nitya','Tech Lead');
+
+insert into customers(customerid, customername, designation)values(01,'Nitya','Tech Lead');
+
+create table customers(customerid int primary key, customername varchar(20),desgination varchar(80), city varchar(50),postalcode varchar(20),country varchar(20));
+
+DESCRIBE customers
+
+
+insert into customers(customerid, customername, desgination)values(01,'Nitya','Tech Lead');
+
+insert into customers(customerid, customername, desgination)values(01,'Nitya','Tech Lead');
+
+create table customers(customerid int primary key, customername varchar(20),desgination varchar(80), city varchar(50),postalcode varchar(20),country varchar(20));
+
+DESCRIBE customers
+
+
+insert into customers(customerid, customername, desgination) values (01,'Nitya','Tech Lead');
+
+insert into customers(customerid, customername, desgination) values (01,'Nitya','Tech Lead');
+
+alter table customers add district varchar(30);
+
+create table orders( 
+    order_no int primary key, purchase_amount float, order_date date, 
+    customer_id int, salesman_id int);
+
+INSERT ALL 
+    INTO orders VALUES(70001, 150.5, TO_DATE('2012/10/05', 'YYYY/MM/DD'), 3005, 5002)  
+    INTO orders VALUES(70009, 270.65, TO_DATE('2012/09/10', 'YYYY/MM/DD'), 3001, 5005) 
+    INTO orders VALUES(70002, 65.26, TO_DATE('2012/10/05', 'YYYY/MM/DD'), 3002, 5001) 
+    INTO orders VALUES(70004, 110.5, TO_DATE('2012/08/17', 'YYYY/MM/DD'), 3009, 5003) 
+    INTO orders VALUES(70007, 948.5, TO_DATE('2012/09/10', 'YYYY/MM/DD'), 3005, 5002) 
+    INTO orders VALUES(70005, 2400.6, TO_DATE('2012/07/27', 'YYYY/MM/DD'), 3007, 5001) 
+    INTO orders VALUES(70008, 5760, TO_DATE('2012/08/15', 'YYYY/MM/DD'), 3002, 5001) 
+    INTO orders VALUES(70010, 1983.43, TO_DATE('2012/10/10', 'YYYY/MM/DD'), 3004, 5006) 
+    INTO orders VALUES(70003, 2480.4, TO_DATE('2012/10/10', 'YYYY/MM/DD'), 3009, 5003) 
+    INTO orders VALUES(70012, 250.45, TO_DATE('2012/06/27', 'YYYY/MM/DD'), 3008, 5002) 
+    INTO orders VALUES(70011, 75.29, TO_DATE('2012/08/17', 'YYYY/MM/DD'), 3003, 5007) 
+    INTO orders VALUES(70013, 3045.6, TO_DATE('2012/04/25', 'YYYY/MM/DD'), 3002, 5001) 
+SELECT 1 FROM DUAL;
+
+select * from orders;
+
